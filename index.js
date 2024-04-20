1dbcd3d38e3f2083bcc3553eb997b04f32458c2f
lukamil1998 <luka_milanovic@rocketmail.com>
2024-04-20 17:40:04 +0200

    Port changes to the file

--- index.js
+++ index.js
@@ -143,5 +143,5 @@ function generateRandomId() {
 // Start the server
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`)
+  console.log(`Server is running on ${PORT}`)
 })
